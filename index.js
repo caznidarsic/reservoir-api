f636639a8623bf12b2852fec58cebb1783b9068e
caznidarsic <cznidar1@jh.edu>
2023-10-24 18:00:31 -0700

    Added endpoints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const { getDateRange, cleanData } = require('./utility');
+const { getMonthlyDateRange, getCurrentYear, cleanData, getCacheTTL } = require('./utility');
 const app = express();
 const redis = require('redis');
 const port = process.env.PORT || 3000;
@@ -23,50 +23,211 @@ let redisClient;
 })();
 
 // function to fetch data from API
-async function fetchApiData(stationid, span) {
-    let url = `https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations=${stationid}&SensorNums=15&dur_code=${(span === '6 months' ? 'D' : 'M')}&${getDateRange(span)}`;
+async function fetchCurrentApiData(stationid, span) {
+    let url = `https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations=${stationid}&SensorNums=15&dur_code=${(span === '6 months' ? 'D' : 'M')}&${getMonthlyDateRange(span)}`;
     const apiResponse = await axios.get(url)
-    return apiResponse.data;
+    let currentResults = cleanData(apiResponse.data, span);
+    return currentResults;
+}
+
+// function to fetch historical data from API
+async function fetchHistoricalApiData(stationid, span) {
+    let url = `https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations=${stationid}&SensorNums=15&dur_code=${(span === '6 months' ? 'D' : 'M')}&Start=1988-1-1&End=${getCurrentYear() - 1}-12-1`;
+    const apiResponse = await axios.get(url);
+    let data = cleanData(apiResponse.data, span);
+    const averages = {};
+    let month;
+
+    data.forEach(item => {
+        month = new Date(item.date).getMonth() + 1;
+        if (!averages[month]) {
+            averages[month] = { month: month, total: 0, count: 0 };
+        }
+        averages[month].total += item.value;
+        averages[month].count += 1;
+    })
+
+    Object.keys(averages).forEach(key => {
+        const item = averages[key];
+        item.average = Math.floor(item.total / item.count);
+    });
+
+    return averages;
 }
 
 // function to call fetchApiData() and perform caching
-async function getResData(req, res) {
-    let stationid = req.query.stationid;
-    let span = req.query.span;
-    let results;
-    let isCached = false;
+async function getResData(stationid, span) {
+    // let stationid = req.query.stationid.split(',');
+    // let span = req.query.span;
+    let currentResults;
+    let historicalResults;
+    // let id = stationids[0]
+
+
+    // try {
+
+    //     const promises = stationids.map(async (id) => {
+
+    //         const currentCacheResults = await redisClient.get(`monthly_storage_${id}_${span}`);
+    //         if (currentCacheResults) {
+    //             currentResults = JSON.parse(currentCacheResults);
+    //         }
+    //         else {
+    //             currentResults = await fetchCurrentApiData(id, span);
+    //             console.log('had to fetch from API (current)')
+    //             if (currentResults.length == 0) {
+    //                 throw "API returned an empty array";
+    //             }
+    //             await redisClient.set(`monthly_storage_${id}_${span}`, JSON.stringify(currentResults), {
+    //                 EX: getCacheTTL('monthly'),
+    //                 NX: true,
+    //             });
+    //         }
+
+
+    //         const historicalCacheResults = await redisClient.get(`monthly_storage_historical_${id}_${span}`);
+    //         if (historicalCacheResults) {
+    //             historicalResults = JSON.parse(historicalCacheResults);
+    //         }
+    //         else {
+    //             historicalResults = await fetchHistoricalApiData(id, span);
+    //             console.log('had to fetch from API (historical)')
+    //             if (!historicalResults) {
+    //                 throw "API returned no data";
+    //             }
+    //             await redisClient.set(`monthly_storage_historical_${id}_${span}`, JSON.stringify(historicalResults), {
+    //                 EX: getCacheTTL('annual'),
+    //                 NX: true,
+    //             });
+    //         }
+
+
+    //         currentResults = cleanData(currentResults, span);
+
+    //         let month
+    //         // add historical averages to the current data
+    //         currentResults.forEach(item => {
+    //             month = new Date(item.date).getMonth() + 1;
+    //             item.average = historicalResults[month].average;
+    //         })
+    //         console.log(currentResults[1])
+    //     })
+
+    //     await Promise.all(promises);
+
+    //     return res.status(200).send(JSON.stringify(currentResults));
+
+    // } catch (error) {
+    //     console.log("error fetching data!!");
+    //     console.log(error)
+    //     return res.status(500).send(JSON.stringify({ message: `error fetching data` }));
+    // }
+
+
 
     try {
-        const cacheResults = await redisClient.get(`${stationid}_${span}`);
-        if (cacheResults) {
-            isCached = true;
-            results = JSON.parse(cacheResults);
+        const currentCacheResults = await redisClient.get(`monthly_storage_${stationid}_${span}`);
+        if (currentCacheResults) {
+            currentResults = JSON.parse(currentCacheResults);
         }
         else {
-            results = await fetchApiData(stationid, span);
-            console.log('had to fetch from API')
-            if (results.length == 0) {
+            currentResults = await fetchCurrentApiData(stationid, span);
+            console.log('had to fetch from API (current)')
+            if (currentResults.length == 0) {
                 throw "API returned an empty array";
             }
-            await redisClient.set(`${stationid}_${span}`, JSON.stringify(results), {
-                EX: 86400,
+            await redisClient.set(`monthly_storage_${stationid}_${span}`, JSON.stringify(currentResults), {
+                EX: getCacheTTL('monthly'),
                 NX: true,
             });
         }
 
-        return res.status(200).send(JSON.stringify(cleanData(results, span)));
+        const historicalCacheResults = await redisClient.get(`monthly_storage_historical_${stationid}_${span}`);
+        if (historicalCacheResults) {
+            historicalResults = JSON.parse(historicalCacheResults);
+        }
+        else {
+            historicalResults = await fetchHistoricalApiData(stationid, span);
+            console.log('had to fetch from API (historical)')
+            if (!historicalResults) {
+                throw "API returned no data";
+            }
+            await redisClient.set(`monthly_storage_historical_${stationid}_${span}`, JSON.stringify(historicalResults), {
+                EX: getCacheTTL('annual'),
+                NX: true,
+            });
+        }
 
+        // currentResults = cleanData(currentResults, span);
+
+        let month
+        // add historical averages to the current data
+        currentResults.forEach(item => {
+            month = new Date(item.date).getMonth() + 1;
+            item.average = historicalResults[month].average;
+        })
+
+        // return res.status(200).send(JSON.stringify(currentResults));
+        return currentResults;
+
+    } catch (error) {
+        console.log("error fetching data!!");
+        console.log(error)
+        // return res.status(500).send(JSON.stringify({ message: `error fetching data` }));
+    }
+
+}
+
+/*
+Function that takes a list of station ids as input, and outputs the total sum of average storage for all stations,
+as well as the current storage for all stations.
+*/
+async function getMultiResData(stationids, span) {
+    let singleResResults;
+    let combinedResults = {};
+
+    for (const id of stationids) {
+        singleResResults = await getResData(id, span);
+
+        singleResResults.forEach(item => {
+
+            month = new Date(item.date).getMonth() + 1;
+            if (!combinedResults[item.date]) {
+                combinedResults[item.date] = { date: item.date, totalAverage: 0 };
+            }
+            combinedResults[item.date][item.stationId] = item.value;
+            combinedResults[item.date].totalAverage += item.average;
+        })
+    }
+
+    const dataList = Object.keys(combinedResults).map((key) => combinedResults[key]);
+
+    return dataList;
+}
+
+
+//endpoint to get monthly reservoir data
+app.get('/resdata', async (req, res) => {
+    let stationids = req.query.stationid.split(',');
+    let span = req.query.span;
+
+    try {
+        if (stationids.length > 1) {
+            let currentResults = await getMultiResData(stationids, span);
+            return res.status(200).send(JSON.stringify(currentResults));
+        }
+
+        else {
+            let currentResults = await getResData(stationids, span);
+            return res.status(200).send(JSON.stringify(currentResults));
+        }
     } catch (error) {
         console.log("error fetching data!!");
         console.log(error)
         return res.status(500).send(JSON.stringify({ message: `error fetching data` }));
     }
-}
 
-
-
-//endpoint to get monthly reservoir data
-app.get('/resdata', getResData);
+});
 
 
 // Start the server
--- utility.js
+++ utility.js
@@ -7,16 +7,63 @@ function formatDateString(dateString) {
     })
 }
 
-function getDateRange(span) {
+function getMonthlyDateRange(span) {
     const currentDate = new Date();
     let year = currentDate.getFullYear();
     let month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    let day = String(currentDate.getDate()).padStart(2, '0');
+    let startMonth;
+    let startYear;
+
+    // because monthly data gets the current month chopped off, we must reach back by an extra month to get a full year's worth of data
+    if (month == 1) {
+        startMonth = 12;
+        startYear = year - span - 1;
+    }
+    else {
+        startMonth = month - 1;
+        startYear = year - span;
+    }
+
+    // let day = String(currentDate.getDate()).padStart(2, '0');
+    let day = 15; // setting to 15 so that no queries are made with days of months that don't exist (for example: February 31)
     let endDate = `${year}-${month}-${day}`;
-    // console.log(chartRange);
-    year = year - span;
-    let startDate = `${year}-${month}-${day}`;
+    let startDate = `${startYear}-${startMonth}-${day}`;
     return `Start=${startDate}&End=${endDate}`
+}
+
+function getCurrentYear() {
+    const currentDate = new Date();
+    let year = currentDate.getFullYear();
+    return `${year}`
+}
+
+/* 
+Function to calculate the number of seconds left in the current year or month (plus 1 hour to give the CDEC API enough time to update)
+Used to set TTL for data in redis cache.
+*/
+function getCacheTTL(interval) {
+    const currentDate = new Date();
+    let secondsLeft;
+    if (interval === 'annual') {
+        const currentYear = currentDate.getFullYear();
+        // Create a new Date object for the start of the next year
+        const nextYearDate = new Date(currentYear + 1, 0, 1, 0, 0, 0);
+        // Calculate the difference in milliseconds between the current date and the start of the next year
+        const timeDifference = nextYearDate - currentDate;
+        // Convert the time difference to seconds
+        secondsLeft = Math.floor(timeDifference / 1000);
+    }
+    else if (interval === 'monthly') {
+        // Get the current month
+        const currentMonth = currentDate.getMonth();
+        // Create a new Date object for the start of the next month
+        const nextMonthDate = new Date(currentDate.getFullYear(), currentMonth + 1, 1, 0, 0, 0);
+        // Calculate the difference in milliseconds between the current date and the start of the next year
+        const timeDifference = nextMonthDate - currentDate;
+        // Convert the time difference to seconds
+        secondsLeft = Math.floor(timeDifference / 1000);
+    }
+    return secondsLeft + 3600; // add 1 hour to give CDEC API enough time to update.
 }
 
 function cleanData(data, span) {
@@ -28,7 +75,7 @@ function cleanData(data, span) {
     }));
 
     //need to remove last element of data if data is sampled monthly, since the API returns 0 for value of current month
-    if (span === '1 year' || span === '2 years') {
+    if (span === '1' || span === '2') {
         data.pop();
     }
 
@@ -55,6 +102,8 @@ function cleanData(data, span) {
 }
 
 module.exports = {
-    getDateRange,
+    getMonthlyDateRange,
+    getCurrentYear,
     cleanData,
+    getCacheTTL,
 }
